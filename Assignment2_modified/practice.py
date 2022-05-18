2a0cce7711496960574bf949851c401c9f90eccb
olccihyeon <sh102201@snu.ac.kr>
2022-05-18 14:13:17 +0900

    hw2

---
+++ Assignment2_modified/practice.py
@@ -0,0 +1,17 @@
+# for i in range(1, 9):
+#     print(i)
+# a = []
+# print(len(a))
+# a.append("a")
+# print(a)
+
+a = [1, 2, 3]
+b = []
+c = [3, 4, 5]
+print(a)
+print(b)
+print(c)
+b = b + c
+b += a
+print(b)
+
